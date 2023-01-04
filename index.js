230005e546100d7a962528d7f38c1c0874a9aa09
T3tje <tilman2013@gmail.com>
2023-01-04 08:27:59 +0100

    toJSON

--- index.js
+++ index.js
@@ -1,69 +1,63 @@
-
 const express = require("express")
 const app = express()
 const fs = require("fs")
+const bodyParser = require("body-parser")
+const jsonParser = bodyParser.json()
 
-
-app.get("/api/allcases", (request,response) => {
-
-   try {
-      const data = fs.readFileSync("./cases.txt", "utf8");
-      response.status(200).send(data)
-    } catch (err) {
-      response.status(404).send(err);
-    }
-
+app.get("/api/allcases", (request, response) => {
+  try {
+    const data = fs.readFileSync("./cases.txt", "utf8")
+    response.status(200).send(data)
+  } catch (err) {
+    response.status(404).send(err)
+  }
 })
 
-app.post("/api/case", (request,response) => {
-      
-   let allCases = []
+app.post("/api/case", jsonParser, (request, response) => {
+  let allCases = []
 
-   try {
-   const data = fs.readFileSync("./cases.txt", "utf8");
-   allCases = JSON.parse(data)
-   } catch (err) {
-   console.error("theres no such file yet");
-   }
+  try {
+    const data = fs.readFileSync("./cases.txt", "utf8")
+    allCases = JSON.parse(data)
+  } catch (err) {
+    console.error("theres no such file yet")
+  }
 
+  const highestNumberArray = allCases.map((item) => parseInt(item.fxFileId.slice(-2)))
 
-   const highestNumberArray = allCases.map(item => parseInt(item.fxFileId.slice(-2)))
-   
-   let highestNumber
-  
-   if (highestNumberArray.length === 0) {
-      highestNumber = 1
-   } else {
-      highestNumber = Math.max(...highestNumberArray) + 1
-   }
+  console.log(Date())
+  let highestNumber
 
-   const newCounter = highestNumber < 10 ? `0${highestNumber}` : `${highestNumber}`
-   
+  if (highestNumberArray.length === 0) {
+    highestNumber = 1
+  } else {
+    highestNumber = Math.max(...highestNumberArray) + 1
+  }
 
-   const newCase = {
-      customerName: request.query.customerName,
-      startDate: request.query.startDate,
-      isFinished: request.query.isFinished === "true" ? true : false,
-      fxFileId: `${request.query.customerName}-${request.query.startDate.substring(8,10)}-${newCounter}`
-   }
-         
-   allCases.push(newCase)
+  const newCounter = highestNumber < 10 ? `0${highestNumber}` : `${highestNumber}`
 
-   
-   const fileText = JSON.stringify(allCases)
-      
-   fs.writeFile("./cases.txt", fileText, (err) => {
-      if(err) {
-         console.error(err);
-         return
-      }
+  const newCase = {
+    customerName: request.body.customerName,
+    startDate: request.body.startDate,
+    isFinished: request.body.isFinished,
+    fxFileId: `${request.body.customerName}-${request.body.startDate.substring(13, 15)}-${newCounter}`,
+  }
 
-      console.log("case successfully added");
-   })
+  allCases.push(newCase)
 
-   response.status(200).json(allCases)
+  const fileText = JSON.stringify(allCases)
 
+  fs.writeFile("./cases.txt", fileText, (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    console.log("case successfully added")
   })
+
+  response.status(200).json(allCases)
+})
 
 const PORT = 3001
 app.listen(PORT)
